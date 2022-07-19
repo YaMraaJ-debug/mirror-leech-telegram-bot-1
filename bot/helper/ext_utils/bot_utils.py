b7315f107cf4f93b14de163aee790146c7b9a7ac
Ronin <75478274+ronin08@users.noreply.github.com>
2022-07-19 08:37:21 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -1,5 +1,4 @@
-import re
-from re import findall as re_findall
+from re import match as re_match, findall as re_findall
 from threading import Thread, Event
 from time import time
 from math import ceil
